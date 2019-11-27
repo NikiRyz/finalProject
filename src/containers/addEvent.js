09773da30c6fd44c9d1f5e4301a17144157bde61
boumrz <boumrz@gmail.com>
2019-11-28 00:03:50 +0300

    delete dead fetch

--- src/containers/addEvent.js
+++ src/containers/addEvent.js
@@ -6,28 +6,13 @@ const AddEventsPage = reduxForm({ form: 'event' })(addEventsPage)
 
 export default class Event extends React.Component {
   render () {
-    const submit = formData =>
-      new Promise((resolve, reject) => {
-        fetch('/events/events_add', {
-          method: 'post',
-          body: JSON.stringify(formData)
-        })
-          .then(res => res.json())
-          .then(res => {
-            if (res.hasOwnProperty('errors')) {
-              reject(res.errors)
-            } else {
-              resolve(res.data)
-            }
-          })
-      })
 
     return (
       <div className="main-container">
         <div id="envelope" className="envelope">
           <b> Добавить мероприятие </b>
 
-          <AddEventsPage onSubmit={submit} />
+          <AddEventsPage />
         </div>
       </div>
     )
